3a0d8f769c770fa9dfc341d9770a1b87ffb6d510
Anita Owens <anita.owens@hyperisland.se>
2022-11-19 17:35:16 +0100

    Add updated files and cheat sheet

--- ETL.py
+++ ETL.py
@@ -1,27 +1,31 @@
 import pandas as pd
 import os
+from datetime import datetime
 # Give the location of your file -
 #file_to_process = ('datasets/Dinner.xlsx')
 print(os.getcwd())
 
+file_to_process = '/Users/anitaowens/Documents/GitHub/Forecasting-in-Python/datasets/creditunion.csv'
 
 
-
-def extract_from_csv(file_to_process): 
+def extract_from_csv(): 
     extracted_data = pd.read_csv(file_to_process) 
     return extracted_data
 
-extract_from_csv('/Users/anitaowens/Documents/GitHub/Forecasting-in-Python/datasets/creditunion.csv')
+#extract_from_csv('file_path)
 
+def transform_data():
+    rows = extract_from_csv()
+    df = pd.DataFrame(rows)
+    df['year'] = 1990
+    df['date_string'] = df['year'].astype(str) + df['MONTH'].astype(str) + df['DAYMON'].astype(str)
+    return df
 
+def load_data():
+    data = transform_data()
+    nrows = data.shape[0]
+    ncols = data.shape[1]
+    print("Finished at {}. Uploaded {} rows and {} columns".format(datetime.today().strftime("%Y-%m-%d %H:%M:%S"), nrows, ncols))
+    #return data
 
-def transform_data(df):
-       df['year'] = 1990
-       df['date_string'] = df['year'].astype(str) + df['MONTH'].astype(str) + df['DAYMON']
-       return df
-
-out = initial_input
-for func in [extract_from_csv, transform]:
-    out = func(out)
-
-transform(extracted_data)
+load_data()
